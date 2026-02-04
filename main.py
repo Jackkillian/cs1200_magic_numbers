53dfb20dbbc62f2507cb9d8afb967c7fa3f878d0
Jack Freund <jackkillian@tuta.com>
2026-02-04 15:16:00 -0600

    attempt to implement multithreading and caching (time: did not finish, ran out of memory)

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-# ideas: cache of numbers that have already been verified to be "magic"
-
-
+import threading
 from time import perf_counter
+
+MAX_VAL = 5000000000
+NUM_THREADS = 10
+STEP = int(MAX_VAL / NUM_THREADS)
+
+lock = threading.RLock()
+cache = {}
+
+# notes from first attempt at multithreading with cache:
+# swap size hit 22 gb (my laptop hit full storage because of the swap size...) before macos killed it
 
 
 def is_magic_number(x):
@@ -9,7 +17,12 @@ def is_magic_number(x):
         return False
     elif x == 1:
         return True
-    elif x % 2:
+
+    with lock:
+        if i in cache:
+            return True
+
+    if x % 2:
         # number is odd
         return is_magic_number(x * 3 + 1)
     else:
@@ -17,16 +30,38 @@ def is_magic_number(x):
         return is_magic_number(x / 2)
 
 
+def calculate_numbers(start, end):
+    print(f"Starting thread from {start} to {end}")
+    start_perf = perf_counter()
+    for i in range(start, end + 1):
+        is_magic = is_magic_number(i)
+
+        if not is_magic:
+            # TODO
+            raise Exception(f"Number {i} is not magic")
+        else:
+            with lock:
+                cache[i] = 0
+    delta = perf_counter() - start_perf
+    print(f"Calculation from {start} to {end} took {delta:.6f} seconds")
+
+
+threads = []
+for i in range(NUM_THREADS):
+    start = (STEP * i) + 1
+    end = STEP * (i + 1)
+    threads.append(
+        threading.Thread(target=calculate_numbers, args=(start, end), daemon=True)
+    )
+
+for thread in threads:
+    thread.start()
+
+print("Calculating...")
 start = perf_counter()
-for i in range(1, 5000000000):
-    # start2 = perf_counter()
-    is_magic = is_magic_number(i)
-    # end2 = perf_counter()
-    # print(f"Calculation took {end2 - start2:.6f} seconds (num {i})")
-    # print(f"{i}: {is_magic}")
-    if not is_magic:
-        raise Exception(f"Not magic: '{i}'")
+for thread in threads:
+    thread.join()
 end = perf_counter()
-delta = end - start
-print("All numbers are magic")
-print(f"Calculation took {delta:.6f} seconds")
+
+print(f"\n\nTotal calculation took {end - start:.6f} seconds")
+print(f"Length of cache: {len(cache)} (is equal to max num? {len(cache) == MAX_VAL})")
