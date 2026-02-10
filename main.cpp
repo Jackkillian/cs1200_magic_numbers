652998ced8b227d631baf84d4770eb927ee8ec05
Jack Freund <jackkillian@tuta.com>
2026-02-09 23:51:01 -0600

    use singlethread with optimization (time: ~75 seconds)

--- main.cpp
+++ main.cpp
@@ -1,28 +1,19 @@
-#include <atomic>
 #include <chrono>
 #include <iomanip>
 #include <ios>
 #include <iostream>
-#include <thread>
-#include <unordered_map>
-using std::atomic;
-using std::atomic_flag;
 using std::cout;
 using std::endl;
-using std::memory_order_acquire;
-using std::memory_order_release;
-using std::thread;
 using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
 
 const int MAX_TRIES = 100000000;
+unsigned long int lastVerifiedNumber = -1;
+const int UPDATE_INTERVAL = 100000000;
 
-atomic<bool> FOUND_NON_MAGIC(false);
-atomic_flag flag = ATOMIC_FLAG_INIT;
-
-bool isMagicNumber(unsigned long int x) {
+bool isMagicNumber(unsigned long x) {
   if (x <= 0) {
     return false;
   }
@@ -32,70 +23,52 @@ bool isMagicNumber(unsigned long int x) {
   }
 
   int tries = 0;
-  unsigned long int original = x;
   while (x != 1) {
     if (x & 1) {
-      // number is odd
+      // number is odd, multiply by 3 and add 1
       x = x * 3 + 1;
     } else {
-      // number is even
+      // number is even, divide by 2
       x = x >> 1;
+    }
+
+    if (x <= lastVerifiedNumber) {
+      // all numbers before the last verified number have been validated as
+      // "magic"
+      return true;
     }
 
     tries++;
     if (tries >= MAX_TRIES) {
-      cout << "Found non-magic number (or hit limit): " << original << endl;
-      FOUND_NON_MAGIC.store(true);
+      cout << "Hit max try limit (" << MAX_TRIES << ")" << endl;
       return false;
     }
   }
-  return true;
-}
 
-void workerFunc(int threadNumber, unsigned long startOffset,
-                unsigned long step) {
-  for (unsigned long int i = 0; i <= step; i++) {
-    unsigned long number = i + startOffset;
-    if (!isMagicNumber(number)) {
-      cout << number << " is not a magic number." << endl;
-      return;
-    } else if (number % 10000000 == 0) {
-      cout << number << " is a magic number! (" << std::fixed
-           << std::setprecision(1) << static_cast<float>(i) / step * 100
-           << "\% done on thread #" << threadNumber << ")" << endl;
-    }
-  }
+  return true;
 }
 
 int main() {
   const unsigned long int END = 5000000000;
   // const unsigned long int END = 500000000;
-  const int NUM_THREADS = 10;
-  const unsigned long int STEP = END / NUM_THREADS;
-  thread threads[NUM_THREADS];
-
-  cout << "Calculated number of threads: " << NUM_THREADS << endl;
-  cout << "Calculated steps: " << STEP << endl;
   cout << "Calculating from 1 to " << END << "...\n" << endl;
 
   auto start_time = high_resolution_clock::now();
-  for (int i = 0; i < NUM_THREADS; i++) {
-    unsigned long int startVal = STEP * i + 1;
-    cout << "Starting thread #" << i + 1 << " for [" << startVal << ", "
-         << startVal + STEP - 1 << "]" << endl;
-    threads[i] = thread(workerFunc, i + 1, startVal, STEP);
-  }
-  cout << endl;
 
-  for (int i = 0; i < NUM_THREADS; i++) {
-    threads[i].join();
+  for (unsigned long int i = 1; i <= END; i++) {
+    if (!isMagicNumber(i)) {
+      cout << i << " is not a magic number." << endl;
+      return 1;
+    } else if (i % UPDATE_INTERVAL == 0) {
+      cout << i << " is a magic number! (" << std::fixed << std::setprecision(1)
+           << static_cast<float>(i) / END * 100 << "\% done)" << endl;
+    }
+    lastVerifiedNumber = i;
   }
 
   auto end_time = high_resolution_clock::now();
   auto ms = duration_cast<milliseconds>(end_time - start_time);
 
-  cout << "Are all numbers magic? (1=True, 0=False): " << !FOUND_NON_MAGIC
-       << endl;
   cout << "Completed in " << ms.count() / 1000.0 << " seconds" << endl;
   cout << "All numbers from 1 to " << END << " are magic!" << endl;
   return 0;
